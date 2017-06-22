08e2d2c1d921d7c56876632733c3cff6c039f2b2
OlaAde <adeogo.oladipo@gmail.com>
2017-06-22 21:36:05 +0300

    initial commit

--- app/src/main/java/com/example/adeogo/bakingapp/ui/ExoFragment.java
+++ app/src/main/java/com/example/adeogo/bakingapp/ui/ExoFragment.java
@@ -1,11 +1,29 @@
 package com.example.adeogo.bakingapp.ui;
 
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.adeogo.bakingapp.R;
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.DefaultRenderersFactory;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 
 import com.example.adeogo.bakingapp.R;
 
@@ -13,6 +31,34 @@ import com.example.adeogo.bakingapp.R;
  * A simple {@link Fragment} subclass.
  */
 public class ExoFragment extends Fragment {
+
+    private SimpleExoPlayerView mPlayerView;
+    private SimpleExoPlayer mPlayer;
+
+    // autoplay = false
+    private boolean autoPlay = false;
+
+    // used to remember the playback position
+    private int currentWindow;
+    private long playbackPosition;
+
+    // constant fields for saving and restoring bundle
+    private static final String AUTOPLAY = "autoplay";
+    private static final String CURRENT_WINDOW_INDEX = "current_window_index";
+    private static final String PLAYBACK_POSITION = "playback_position";
+
+    private Context mContext;
+
+    private static String mUrlVideo = null;
+    private static String mDescription = "Here we have it ncsblbhl.dg";
+    // sample audio for testing exoplayer
+    private static final String NIGERIA_NATIONAL_ANTHEM_MP3 = "http://www.noiseaddicts.com/samples_1w72b820/4237.mp3";
+
+    // sample videos for testing exoplayer
+    private static final String VIDEO_1 = "http://techslides.com/demos/sample-videos/small.mp4";
+    private static final String VIDEO_2 = "http://clips.vorwaerts-gmbh.de/VfE_html5.mp4";
+    private  TextView mDescTextView;
+
 
 
     public ExoFragment() {
@@ -24,7 +70,145 @@ public class ExoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_exo, container, false);
+        // find the view responsible for visual feedback (media controls & display)
+        View rootView = inflater.inflate(R.layout.fragment_exo, container, false);
+        mContext = getContext();
+        mPlayerView = (SimpleExoPlayerView) rootView.findViewById(R.id.media_view);
+        mDescTextView = (TextView) rootView.findViewById(R.id.full_description_tv);
+        mDescTextView.setText(mDescription);
+
+
+
+
+        // if we have saved player state, restore it
+        if (savedInstanceState != null) {
+            playbackPosition = savedInstanceState.getLong(PLAYBACK_POSITION, 0);
+            currentWindow = savedInstanceState.getInt(CURRENT_WINDOW_INDEX, 0);
+            autoPlay = savedInstanceState.getBoolean(AUTOPLAY, false);
+        }
+        return rootView;
     }
+
+    public void sendToExoFrag(String VideoUrl, String Description){
+        mUrlVideo = VideoUrl;
+        mDescription = Description;
+    }
+
+    void initializePlayer() {
+        // create a new instance of SimpleExoPlayer
+        mPlayer = ExoPlayerFactory.newSimpleInstance(
+                new DefaultRenderersFactory(mContext),
+                new DefaultTrackSelector(),
+                new DefaultLoadControl());
+
+        // attach the just created player to the view responsible for displaying the media (i.e. media controls, visual feedback)
+        mPlayerView.setPlayer(mPlayer);
+        mPlayer.setPlayWhenReady(autoPlay);
+
+        // resume playback position
+        mPlayer.seekTo(currentWindow, playbackPosition);
+
+        Uri uri = Uri.parse(mUrlVideo);
+        MediaSource mediaSource = buildMediaSource(uri);
+
+        // now we are ready to start playing our media files
+        mPlayer.prepare(mediaSource);
+    }
+
+    private MediaSource buildMediaSource(Uri uri) {
+        DefaultExtractorsFactory extractorSourceFactory = new DefaultExtractorsFactory();
+        DefaultHttpDataSourceFactory dataSourceFactory = new DefaultHttpDataSourceFactory("ua");
+
+        ExtractorMediaSource audioSource = new ExtractorMediaSource(uri, dataSourceFactory, extractorSourceFactory, null, null);
+
+        // this return a single mediaSource object. i.e. no next, previous buttons to play next/prev media file
+        return new ExtractorMediaSource(uri, dataSourceFactory, extractorSourceFactory, null, null);
+
+        /*
+         * Uncomment the line below to play multiple meidiaSources in sequence aka playlist (and totally without buffering!)
+         * NOTE: you have to comment the return statement just above this comment
+         */
+
+        /*
+          ExtractorMediaSource videoSource1 = new ExtractorMediaSource(Uri.parse(VIDEO_1), dataSourceFactory, extractorSourceFactory, null, null);
+          ExtractorMediaSource videoSource2 = new ExtractorMediaSource(Uri.parse(VIDEO_2), dataSourceFactory, extractorSourceFactory, null, null);
+          // returns a mediaSource collection
+          return new ConcatenatingMediaSource(videoSource1, audioSource, videoSource2);
+         */
+
+    }
+
+
+    private void releasePlayer() {
+        if (mPlayer != null) {
+            // save the player state before releasing its resources
+            playbackPosition = mPlayer.getCurrentPosition();
+            currentWindow = mPlayer.getCurrentWindowIndex();
+            autoPlay = mPlayer.getPlayWhenReady();
+            mPlayer.release();
+            mPlayer = null;
+        }
+    }
+
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if (mPlayer == null) {
+            outState.putLong(PLAYBACK_POSITION, playbackPosition);
+            outState.putInt(CURRENT_WINDOW_INDEX, currentWindow);
+            outState.putBoolean(AUTOPLAY, autoPlay);
+        }
+    }
+
+    // This is just an implementation detail to have a pure full screen experience. Nothing fancy here
+    @SuppressLint("InlinedApi")
+    private void hideSystemUi() {
+        mPlayerView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (Util.SDK_INT > 23) {
+            initializePlayer();
+
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // start in pure full screen
+        hideSystemUi();
+        if ((Util.SDK_INT <= 23 || mPlayer == null)) {
+            initializePlayer();
+            mDescTextView.setText(mDescription);
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (Util.SDK_INT <= 23) {
+            releasePlayer();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (Util.SDK_INT > 23) {
+            releasePlayer();
+        }
+    }
+
+
+
 
 }
